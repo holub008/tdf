41fcf7e018dd448bc9ceb69cb3dd379066d27202
Ben Mullin <benjamin.t.mullin@gmail.com>
2026-01-07 22:41:25 -0600

    Copy/paste/replace of orchestrate.2425 to 2526 version and creation of bcfk event.

---
+++ orchestrate/s2526/__init__.py
@@ -0,0 +1,32 @@
+from enum import auto, Enum
+import polars as pl
+
+from tdfio.const import Gender
+
+
+class Event(Enum):
+    bcfk = auto()
+
+    def to_string(self) -> str:
+        return self.name
+
+    @staticmethod
+    def from_string(s: str):
+        if s == 'bcfk':
+            return Event.bcfk
+
+        raise ValueError(f'Unrecognized event string representation {s}')
+
+    def save_df(self, df: pl.DataFrame, g: Gender):
+        pretty_df = df.sort(by='age_advantage_event_points', descending=True)
+        selections = ['gender_place', 'first_name', 'last_name', 'age', 'age_advantage_event_points']
+        if 'location' in df.columns:
+            selections.append('location')
+
+        pretty_df\
+            .select(selections)\
+            .write_csv(f'./db/s2526/{self.to_string()}_{g.to_string()}.csv')
+
+
+NON_MAIN_RACE_POINTS = 20.0
+NON_MAIN_EVENT_SPOOF = 999
